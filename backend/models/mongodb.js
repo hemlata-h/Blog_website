439acf73e43b0f9f18e0408918acfdf1e6ac64f2
hemlata-h <156098209+hemlata-h@users.noreply.github.com>
2024-07-17 17:29:40 +0530

    Update mongodb.js

--- backend/models/mongodb.js
+++ backend/models/mongodb.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-
-// Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/Blog")
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.error("MongoDB connection error:", err));
 
-// Define the schema
+
 const ProductSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -31,7 +29,7 @@ const ProductSchema = new mongoose.Schema({
   }
 });
 
-// Middleware to generate slug before validation
+
 ProductSchema.pre('validate', function(next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true });
@@ -39,7 +37,7 @@ ProductSchema.pre('validate', function(next) {
   next();
 });
 
-// Create the model
+
 const Product = mongoose.model('BlogDetails', ProductSchema);
 
 module.exports = Product;
